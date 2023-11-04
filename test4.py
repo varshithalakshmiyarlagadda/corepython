080ec19d9d39ca8e2aa9de5d186b8c898270cf9c
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-04 13:10:38 +0530

    first commit

---
+++ test4.py
@@ -0,0 +1,7 @@
+from turtle import*
+right(75)
+forward(100)
+for i in range(4):
+    right(144)
+    forward(100)
+done()
---
+++ test5.py
@@ -0,0 +1,8 @@
+from turtle import*
+num_sides=6
+side_length=70
+angle=360.0/num_sides
+for i in range(num_sides):
+     forward(side_length)
+     right(angle)
+done()
